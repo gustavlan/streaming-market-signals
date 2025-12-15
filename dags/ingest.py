3a2f6609632878fc00170e1056fe91399d3258e5
gustavlan <gustavl@live.com>
2025-12-15 20:37:06 +0000

    added enrich metadata and build dbt model tasks

--- dags/ingest.py
+++ dags/ingest.py
@@ -14,16 +14,36 @@ default_args = {
 with DAG(
     'music_market_share_ingest',
     default_args=default_args,
-    description='DAG to ingest music market share data from kworb.net',
+    description='Daily pipeline: scrape charts, enrich metadata via Spotify API, build dbt marts',
     schedule_interval='0 9 * * *',
     start_date=datetime(2024, 1, 1),
     catchup=False,
-    tags=['music', 'ingestion'],
+    tags=['music', 'ingestion', 'dbt'],
 ) as dag:
-    
+
     scrape_kworb_task = BashOperator(
         task_id='scrape_kworb_data',
         bash_command='python /opt/airflow/scripts/scrape_kworb.py'
     )
 
-    scrape_kworb_task
+    enrich_metadata_task = BashOperator(
+        task_id='enrich_track_metadata',
+        bash_command='python /opt/airflow/scripts/enrich_metadata.py'
+    )
+
+    build_dbt_models_task = BashOperator(
+        task_id='build_dbt_models',
+        bash_command=(
+            'dbt run '
+            '--project-dir /opt/airflow/dbt_project '
+            '--profiles-dir /opt/airflow/dbt_project '
+            '--select '
+            'stg_combined_charts '
+            'stg_track_metadata_normalized '
+            'stg_dim_labels_normalized '
+            'int_charts_with_track_id '
+            'fact_market_share'
+        ),
+    )
+
+    scrape_kworb_task >> enrich_metadata_task >> build_dbt_models_task
