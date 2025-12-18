2beeb756b8457bc311cfc8921a762eb1e5ba1f10
gustavlan <gustavl@live.com>
2025-12-18 16:32:23 +0000

    query to match normalised against spotify

---
+++ dbt_project/models/staging/stg_dim_labels_normalized.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='view') }}
+
+with labels as (
+    select * from {{ ref('dim_labels') }}
+)
+
+select
+    label_id,
+    label_name,
+    ultimate_parent_name,
+    market_share_group,
+    -- Normalized key for matching against Spotify labels
+    {{ normalize_label('label_name') }} as normalized_label
+from labels
